b5ab373ad19a37f9be5afc12de9851c5f7c71246
niallstack <niallstack@outlook.com>
2015-11-30 12:00:37 +0000

    Updated Register Student form

--- Playground_Home/RegisterStudentFrm.Designer.cs
+++ Playground_Home/RegisterStudentFrm.Designer.cs
@@ -29,6 +29,32 @@
         private void InitializeComponent()
         {
             this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.studIDTxt = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.stuIDLbl = new System.Windows.Forms.Label();
+            this.forenameLbl = new System.Windows.Forms.Label();
+            this.forenameTxt = new System.Windows.Forms.TextBox();
+            this.surnameLbl = new System.Windows.Forms.Label();
+            this.surnameTxt = new System.Windows.Forms.TextBox();
+            this.dobLbl = new System.Windows.Forms.Label();
+            this.dobTP = new System.Windows.Forms.DateTimePicker();
+            this.groupIDlbl = new System.Windows.Forms.Label();
+            this.groupIDTxt = new System.Windows.Forms.TextBox();
+            this.genderLbl = new System.Windows.Forms.Label();
+            this.imageLbl = new System.Windows.Forms.Label();
+            this.imgTxt = new System.Windows.Forms.TextBox();
+            this.passwordLbl = new System.Windows.Forms.Label();
+            this.passwordTxt = new System.Windows.Forms.TextBox();
+            this.studNumTxt = new System.Windows.Forms.Label();
+            this.stuNumTxt = new System.Windows.Forms.TextBox();
+            this.genCBM = new System.Windows.Forms.ComboBox();
+            this.regBtn = new System.Windows.Forms.Button();
+            this.regStuLinkLbl = new System.Windows.Forms.LinkLabel();
+            this.delStuLinkLbl = new System.Windows.Forms.LinkLabel();
+            this.editStuLinkLbl = new System.Windows.Forms.LinkLabel();
+            this.logoutBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip
@@ -41,15 +67,289 @@
             this.menuStrip.Text = "menuStrip";
             this.menuStrip.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip_ItemClicked);
             // 
+            // studIDTxt
+            // 
+            this.studIDTxt.Location = new System.Drawing.Point(440, 50);
+            this.studIDTxt.Name = "studIDTxt";
+            this.studIDTxt.ReadOnly = true;
+            this.studIDTxt.Size = new System.Drawing.Size(200, 20);
+            this.studIDTxt.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.OrangeRed;
+            this.panel1.Controls.Add(this.logoutBtn);
+            this.panel1.Controls.Add(this.editStuLinkLbl);
+            this.panel1.Controls.Add(this.delStuLinkLbl);
+            this.panel1.Controls.Add(this.regStuLinkLbl);
+            this.panel1.Location = new System.Drawing.Point(0, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(137, 483);
+            this.panel1.TabIndex = 3;
+            // 
+            // stuIDLbl
+            // 
+            this.stuIDLbl.AutoSize = true;
+            this.stuIDLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.stuIDLbl.ForeColor = System.Drawing.Color.OrangeRed;
+            this.stuIDLbl.Location = new System.Drawing.Point(315, 50);
+            this.stuIDLbl.Name = "stuIDLbl";
+            this.stuIDLbl.Size = new System.Drawing.Size(87, 20);
+            this.stuIDLbl.TabIndex = 4;
+            this.stuIDLbl.Text = "Student ID";
+            // 
+            // forenameLbl
+            // 
+            this.forenameLbl.AutoSize = true;
+            this.forenameLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.forenameLbl.ForeColor = System.Drawing.Color.OrangeRed;
+            this.forenameLbl.Location = new System.Drawing.Point(320, 91);
+            this.forenameLbl.Name = "forenameLbl";
+            this.forenameLbl.Size = new System.Drawing.Size(82, 20);
+            this.forenameLbl.TabIndex = 6;
+            this.forenameLbl.Text = "Forename";
+            // 
+            // forenameTxt
+            // 
+            this.forenameTxt.Location = new System.Drawing.Point(440, 93);
+            this.forenameTxt.Name = "forenameTxt";
+            this.forenameTxt.Size = new System.Drawing.Size(200, 20);
+            this.forenameTxt.TabIndex = 5;
+            // 
+            // surnameLbl
+            // 
+            this.surnameLbl.AutoSize = true;
+            this.surnameLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.surnameLbl.ForeColor = System.Drawing.Color.OrangeRed;
+            this.surnameLbl.Location = new System.Drawing.Point(328, 132);
+            this.surnameLbl.Name = "surnameLbl";
+            this.surnameLbl.Size = new System.Drawing.Size(74, 20);
+            this.surnameLbl.TabIndex = 8;
+            this.surnameLbl.Text = "Surname";
+            // 
+            // surnameTxt
+            // 
+            this.surnameTxt.Location = new System.Drawing.Point(440, 132);
+            this.surnameTxt.Name = "surnameTxt";
+            this.surnameTxt.Size = new System.Drawing.Size(200, 20);
+            this.surnameTxt.TabIndex = 7;
+            // 
+            // dobLbl
+            // 
+            this.dobLbl.AutoSize = true;
+            this.dobLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dobLbl.ForeColor = System.Drawing.Color.OrangeRed;
+            this.dobLbl.Location = new System.Drawing.Point(358, 174);
+            this.dobLbl.Name = "dobLbl";
+            this.dobLbl.Size = new System.Drawing.Size(44, 20);
+            this.dobLbl.TabIndex = 10;
+            this.dobLbl.Text = "DOB";
+            // 
+            // dobTP
+            // 
+            this.dobTP.Location = new System.Drawing.Point(440, 174);
+            this.dobTP.Name = "dobTP";
+            this.dobTP.Size = new System.Drawing.Size(200, 20);
+            this.dobTP.TabIndex = 11;
+            // 
+            // groupIDlbl
+            // 
+            this.groupIDlbl.AutoSize = true;
+            this.groupIDlbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupIDlbl.ForeColor = System.Drawing.Color.OrangeRed;
+            this.groupIDlbl.Location = new System.Drawing.Point(327, 214);
+            this.groupIDlbl.Name = "groupIDlbl";
+            this.groupIDlbl.Size = new System.Drawing.Size(75, 20);
+            this.groupIDlbl.TabIndex = 13;
+            this.groupIDlbl.Text = "Group ID";
+            // 
+            // groupIDTxt
+            // 
+            this.groupIDTxt.Location = new System.Drawing.Point(440, 214);
+            this.groupIDTxt.Name = "groupIDTxt";
+            this.groupIDTxt.ReadOnly = true;
+            this.groupIDTxt.Size = new System.Drawing.Size(200, 20);
+            this.groupIDTxt.TabIndex = 12;
+            // 
+            // genderLbl
+            // 
+            this.genderLbl.AutoSize = true;
+            this.genderLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.genderLbl.ForeColor = System.Drawing.Color.OrangeRed;
+            this.genderLbl.Location = new System.Drawing.Point(339, 255);
+            this.genderLbl.Name = "genderLbl";
+            this.genderLbl.Size = new System.Drawing.Size(63, 20);
+            this.genderLbl.TabIndex = 15;
+            this.genderLbl.Text = "Gender";
+            // 
+            // imageLbl
+            // 
+            this.imageLbl.AutoSize = true;
+            this.imageLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.imageLbl.ForeColor = System.Drawing.Color.OrangeRed;
+            this.imageLbl.Location = new System.Drawing.Point(348, 300);
+            this.imageLbl.Name = "imageLbl";
+            this.imageLbl.Size = new System.Drawing.Size(54, 20);
+            this.imageLbl.TabIndex = 17;
+            this.imageLbl.Text = "Image";
+            // 
+            // imgTxt
+            // 
+            this.imgTxt.Location = new System.Drawing.Point(440, 300);
+            this.imgTxt.Name = "imgTxt";
+            this.imgTxt.ReadOnly = true;
+            this.imgTxt.Size = new System.Drawing.Size(200, 20);
+            this.imgTxt.TabIndex = 16;
+            // 
+            // passwordLbl
+            // 
+            this.passwordLbl.AutoSize = true;
+            this.passwordLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLbl.ForeColor = System.Drawing.Color.OrangeRed;
+            this.passwordLbl.Location = new System.Drawing.Point(324, 337);
+            this.passwordLbl.Name = "passwordLbl";
+            this.passwordLbl.Size = new System.Drawing.Size(78, 20);
+            this.passwordLbl.TabIndex = 19;
+            this.passwordLbl.Text = "Password";
+            // 
+            // passwordTxt
+            // 
+            this.passwordTxt.Location = new System.Drawing.Point(440, 337);
+            this.passwordTxt.Name = "passwordTxt";
+            this.passwordTxt.ReadOnly = true;
+            this.passwordTxt.Size = new System.Drawing.Size(200, 20);
+            this.passwordTxt.TabIndex = 18;
+            // 
+            // studNumTxt
+            // 
+            this.studNumTxt.AutoSize = true;
+            this.studNumTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.studNumTxt.ForeColor = System.Drawing.Color.OrangeRed;
+            this.studNumTxt.Location = new System.Drawing.Point(276, 376);
+            this.studNumTxt.Name = "studNumTxt";
+            this.studNumTxt.Size = new System.Drawing.Size(126, 20);
+            this.studNumTxt.TabIndex = 21;
+            this.studNumTxt.Text = "Student Number";
+            // 
+            // stuNumTxt
+            // 
+            this.stuNumTxt.Location = new System.Drawing.Point(440, 376);
+            this.stuNumTxt.Name = "stuNumTxt";
+            this.stuNumTxt.ReadOnly = true;
+            this.stuNumTxt.Size = new System.Drawing.Size(200, 20);
+            this.stuNumTxt.TabIndex = 20;
+            // 
+            // genCBM
+            // 
+            this.genCBM.FormattingEnabled = true;
+            this.genCBM.Location = new System.Drawing.Point(440, 254);
+            this.genCBM.Name = "genCBM";
+            this.genCBM.Size = new System.Drawing.Size(121, 21);
+            this.genCBM.TabIndex = 22;
+            // 
+            // regBtn
+            // 
+            this.regBtn.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.regBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.regBtn.ForeColor = System.Drawing.Color.OrangeRed;
+            this.regBtn.Location = new System.Drawing.Point(468, 437);
+            this.regBtn.Name = "regBtn";
+            this.regBtn.Size = new System.Drawing.Size(122, 43);
+            this.regBtn.TabIndex = 23;
+            this.regBtn.Text = "Confirm";
+            this.regBtn.UseVisualStyleBackColor = false;
+            // 
+            // regStuLinkLbl
+            // 
+            this.regStuLinkLbl.AutoSize = true;
+            this.regStuLinkLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.regStuLinkLbl.LinkColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.regStuLinkLbl.Location = new System.Drawing.Point(3, 23);
+            this.regStuLinkLbl.Name = "regStuLinkLbl";
+            this.regStuLinkLbl.Size = new System.Drawing.Size(117, 18);
+            this.regStuLinkLbl.TabIndex = 0;
+            this.regStuLinkLbl.TabStop = true;
+            this.regStuLinkLbl.Text = "Register Student";
+            // 
+            // delStuLinkLbl
+            // 
+            this.delStuLinkLbl.AutoSize = true;
+            this.delStuLinkLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.delStuLinkLbl.LinkColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.delStuLinkLbl.Location = new System.Drawing.Point(3, 64);
+            this.delStuLinkLbl.Name = "delStuLinkLbl";
+            this.delStuLinkLbl.Size = new System.Drawing.Size(118, 18);
+            this.delStuLinkLbl.TabIndex = 1;
+            this.delStuLinkLbl.TabStop = true;
+            this.delStuLinkLbl.Text = "Remove Student";
+            // 
+            // editStuLinkLbl
+            // 
+            this.editStuLinkLbl.AutoSize = true;
+            this.editStuLinkLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editStuLinkLbl.LinkColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.editStuLinkLbl.Location = new System.Drawing.Point(3, 104);
+            this.editStuLinkLbl.Name = "editStuLinkLbl";
+            this.editStuLinkLbl.Size = new System.Drawing.Size(87, 18);
+            this.editStuLinkLbl.TabIndex = 2;
+            this.editStuLinkLbl.TabStop = true;
+            this.editStuLinkLbl.Text = "Edit Student";
+            // 
+            // logoutBtn
+            // 
+            this.logoutBtn.BackColor = System.Drawing.Color.OrangeRed;
+            this.logoutBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.logoutBtn.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.logoutBtn.Location = new System.Drawing.Point(6, 430);
+            this.logoutBtn.Name = "logoutBtn";
+            this.logoutBtn.Size = new System.Drawing.Size(122, 43);
+            this.logoutBtn.TabIndex = 24;
+            this.logoutBtn.Text = "Log Out";
+            this.logoutBtn.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGray;
+            this.label1.Location = new System.Drawing.Point(143, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 24);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Register Student";
+            // 
             // RegisterStudentFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.ClientSize = new System.Drawing.Size(694, 512);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.regBtn);
+            this.Controls.Add(this.genCBM);
+            this.Controls.Add(this.studNumTxt);
+            this.Controls.Add(this.stuNumTxt);
+            this.Controls.Add(this.passwordLbl);
+            this.Controls.Add(this.passwordTxt);
+            this.Controls.Add(this.imageLbl);
+            this.Controls.Add(this.imgTxt);
+            this.Controls.Add(this.genderLbl);
+            this.Controls.Add(this.groupIDlbl);
+            this.Controls.Add(this.groupIDTxt);
+            this.Controls.Add(this.dobTP);
+            this.Controls.Add(this.dobLbl);
+            this.Controls.Add(this.surnameLbl);
+            this.Controls.Add(this.surnameTxt);
+            this.Controls.Add(this.forenameLbl);
+            this.Controls.Add(this.forenameTxt);
+            this.Controls.Add(this.stuIDLbl);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.studIDTxt);
             this.Controls.Add(this.menuStrip);
             this.Name = "RegisterStudentFrm";
             this.Text = "Register Student";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -58,5 +358,30 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.TextBox studIDTxt;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label stuIDLbl;
+        private System.Windows.Forms.Label forenameLbl;
+        private System.Windows.Forms.TextBox forenameTxt;
+        private System.Windows.Forms.Label surnameLbl;
+        private System.Windows.Forms.TextBox surnameTxt;
+        private System.Windows.Forms.Label dobLbl;
+        private System.Windows.Forms.DateTimePicker dobTP;
+        private System.Windows.Forms.Label groupIDlbl;
+        private System.Windows.Forms.TextBox groupIDTxt;
+        private System.Windows.Forms.Label genderLbl;
+        private System.Windows.Forms.Label imageLbl;
+        private System.Windows.Forms.TextBox imgTxt;
+        private System.Windows.Forms.Label passwordLbl;
+        private System.Windows.Forms.TextBox passwordTxt;
+        private System.Windows.Forms.Label studNumTxt;
+        private System.Windows.Forms.TextBox stuNumTxt;
+        private System.Windows.Forms.ComboBox genCBM;
+        private System.Windows.Forms.Button regBtn;
+        private System.Windows.Forms.Button logoutBtn;
+        private System.Windows.Forms.LinkLabel editStuLinkLbl;
+        private System.Windows.Forms.LinkLabel delStuLinkLbl;
+        private System.Windows.Forms.LinkLabel regStuLinkLbl;
+        private System.Windows.Forms.Label label1;
     }
 }
