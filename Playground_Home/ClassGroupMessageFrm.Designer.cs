56858dacacf25feb70cd81969aaf9a4c05b6ca92
niallstack <niallstack@outlook.com>
2015-12-01 09:29:33 +0000

    Added Private Message form

--- Playground_Home/ClassGroupMessageFrm.Designer.cs
+++ Playground_Home/ClassGroupMessageFrm.Designer.cs
@@ -40,11 +40,13 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.label1.Location = new System.Drawing.Point(11, 401);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(135, 25);
             this.label1.TabIndex = 14;
             this.label1.Text = "My Message";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // button1
             // 
@@ -57,6 +59,7 @@
             this.button1.TabIndex = 13;
             this.button1.Text = "Send";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // richTextBox1
             // 
@@ -65,16 +68,19 @@
             this.richTextBox1.Size = new System.Drawing.Size(516, 96);
             this.richTextBox1.TabIndex = 12;
             this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.label2.Location = new System.Drawing.Point(12, 25);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(197, 24);
             this.label2.TabIndex = 11;
             this.label2.Text = "My Class Group\'s Wall";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // listView1
             // 
@@ -83,6 +89,7 @@
             this.listView1.Size = new System.Drawing.Size(666, 324);
             this.listView1.TabIndex = 10;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
             // 
             // menuStrip
             // 
@@ -92,6 +99,7 @@
             this.menuStrip.Size = new System.Drawing.Size(666, 24);
             this.menuStrip.TabIndex = 9;
             this.menuStrip.Text = "menuStrip";
+            this.menuStrip.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip_ItemClicked);
             // 
             // ClassGroupMessageFrm
             // 
@@ -107,6 +115,7 @@
             this.Controls.Add(this.menuStrip);
             this.Name = "ClassGroupMessageFrm";
             this.Text = "ClassGroupMessageFrm";
+            this.Load += new System.EventHandler(this.ClassGroupMessageFrm_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Playground_Home/ClassGroupMessageFrm.cs
+++ Playground_Home/ClassGroupMessageFrm.cs
@@ -16,5 +16,40 @@ namespace Playground_Home
         {
             InitializeComponent();
         }
+
+        private void ClassGroupMessageFrm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
