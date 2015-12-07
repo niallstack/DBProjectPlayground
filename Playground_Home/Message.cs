ce8cff67761a26e9d2996971fda6eda5be1f8356
Quentin <qmarechal@hotmail.fr>
2015-12-07 12:04:00 +0000

    Added Message class and messageDAO class

---
+++ Playground_Home/Message.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground_Home
+{
+    class Message
+    {
+        private String messageDetail;
+        private Student autor;
+
+   
+
+        public String MessageDetail
+        {
+            get { return messageDetail; }
+            set { messageDetail = value; }
+        }
+
+        public Student Autor
+        {
+            get { return autor; }
+            set { autor = value; }
+        }
+
+
+
+    }
+}
---
+++ Playground_Home/MessageDAO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OracleClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground_Home
+{
+    class MessageDAO
+    {
+
+        private OracleConnection connection=null;
+
+        public OracleConnection Connection
+        {
+            get { return connection; }
+            set { connection = value; }
+        }
+
+     
+        public MessageDAO(String username, String password)
+        {
+            Connection = new OracleConnection("Data Source=cp3dbinstance.c4pxnpz4ojk8.us-east-1.rds.amazonaws.com:1521/cp3db;" + "User ID=sw4 ;Password=sw4");
+            Connection.Open();
+        }
+      
+
+
+
+
+        public Boolean InsertMessage(Message message)
+        {
+            OracleCommand command = Connection.CreateCommand();
+            OracleCommand co = new OracleCommand("Select * from truc", Connection);
+
+            return true;
+        }
+    }
+}
